43648752857e539021b969a16c07ab48d1d0b483
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-05-11 11:50:23 +0530

    Graph Api's added

--- src/request_handler/request_handler.js
+++ src/request_handler/request_handler.js
@@ -2,14 +2,20 @@ const config = require("../../config/config");
 const startTokenDataHandler = require("./transaction");
 const startOvenDataHandler = require("./ovens");
 const startMainDataHandler = require("./main_data");
+const startTvlDataHandler = require("./tvl");
+const startVolumeDataHandler = require("./volume");
 
 module.exports.startRequestHandler = () => {
     startTokenDataHandler();
     startOvenDataHandler();
     startMainDataHandler();
+    startTvlDataHandler();
+    startVolumeDataHandler();
     setInterval(() => {
         startTokenDataHandler();
         startOvenDataHandler();
         startMainDataHandler();
+        startTvlDataHandler();
+        startVolumeDataHandler();
     }, config.SET_TIMEOUT);
 };
